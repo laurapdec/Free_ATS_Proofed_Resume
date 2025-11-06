8ba792987696113dceab4c1864e5ca676da391b5
Laura Mac <laurapdec@hotmail.com>
2025-11-06 12:09:07 +0100

    feat: add @vercel/analytics dependency and integrate analytics component in _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { AppWrapper } from '../src/components/AppWrapper';
 import { ErrorBoundary } from '../components/ErrorBoundary';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <AppWrapper>
         <Component {...pageProps} />
       </AppWrapper>
+      <Analytics />
     </ErrorBoundary>
   );
 }
